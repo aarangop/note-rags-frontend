6b0dcd2cc3f0b3605b9aa5d2a83d40cf6b89fbd9
Andres Arango <andresap@proton.me>
2025-07-21 21:22:28 -0500

    Update env apiConfig

--- src/lib/config/api.ts
+++ src/lib/config/api.ts
@@ -3,13 +3,11 @@ import { env as publicEnv } from '$env/dynamic/public';
 interface ApiConfig {
 	genAIAPIUrl: string;
 	notesAPIUrl: string;
-	openApiUrl: string;
 }
 
 export const apiConfig: ApiConfig = {
-	genAIAPIUrl: publicEnv.PUBLIC_API_BASE_URL || 'http://localhost:8000',
-	notesAPIUrl: publicEnv.PUBLIC_NOTES_API_BASE_URL || 'http://localhost:8001',
-	openApiUrl: publicEnv.PUBLIC_API_OPENAPI_URL || 'http://localhost:8000/openapi.json'
+	genAIAPIUrl: publicEnv.PUBLIC_GENAI_API_BASE_URL || 'http://localhost:8000',
+	notesAPIUrl: publicEnv.PUBLIC_NOTES_API_BASE_URL || 'http://localhost:8001'
 };
 
 export default apiConfig;
